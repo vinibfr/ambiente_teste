1a4bbbec6e7a0676b9c9a39a0f6b910852040e50
“Vinícius <vinibfr@gmail.com>
2017-06-21 19:17:49 -0300

    Traducão 1

--- projectAlpha/personal/views.py
+++ projectAlpha/personal/views.py
@@ -4,4 +4,4 @@ def index(request):
     return render(request, 'personal/home.html')
 
 def contact(request):
-    return render(request, 'personal/basic.html', {'content':['If you would like to contact me, please email me','vinibfr@gmail.com']})    
+    return render(request, 'personal/basic.html', {'content':['Se quiser falar comigo, tentae pelo e-mail:','vinibfr@gmail.com']})    
