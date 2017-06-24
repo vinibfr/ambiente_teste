b7104a5d2c565e953c8296f993c6031fab891bea
“Vinícius <vinibfr@gmail.com>
2017-06-24 16:46:56 -0300

    alterado header/blog

--- projectAlpha/webapp/views.py
+++ projectAlpha/webapp/views.py
@@ -4,4 +4,4 @@ def index(request):
     return render(request, 'webapp/home.html')
 
 def contact(request):
-    return render(request, 'webapp/basic.html', {'content':['Se quiser falar comigo, tentae pelo e-mail:','vinibfr@gmail.com']})    
+    return render(request, 'webapp/contact.html')    
